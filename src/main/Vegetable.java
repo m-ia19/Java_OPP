d8a0cfea5e7c9f520552002983445e1741026dc4
Maria Zeller <maria.zeller19@web.de>
2022-05-09 14:41:23 +0200

    Edible method

--- src/main/Vegetable.java
+++ src/main/Vegetable.java
@@ -3,5 +3,17 @@ package main;
 public class Vegetable {
 	public String name;
     public int kcal; // per 100g
+	private boolean edible;
+
+    
+    public Vegetable(String _name, int _kcal){
+    	name = _name;
+    	kcal = _kcal;
+    	this.edible = false;
+        }
+    
+    public boolean isEdible() {
+    	return edible;
+    }
 
 }
